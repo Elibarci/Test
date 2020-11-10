9aa672d17102d517b298d24b409d46c77a29d576
Elibarci <elibarci@hotmail.es>
2020-11-10 20:34:09 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Elias
         </a>
       </header>
     </div>
