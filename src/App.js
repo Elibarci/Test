2eca82be536f12a925bcce499617731d6e51dbe7
Elias Barbero <elibarci@hotmail.es>
2021-02-10 20:39:53 +0100

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Primer repositorio de Elias
+          repositorio de Elias
         </p>
         <a
           className="App-link"
